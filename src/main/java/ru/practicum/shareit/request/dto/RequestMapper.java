a35577a5db028559a0f0e5751de460f740915d23
Алексей Панченко <panchpanch@hotmail.com>
2025-03-26 03:43:21 +0300

    рефакторинг

--- src/main/java/ru/practicum/shareit/request/dto/RequestMapper.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.request.dto;
-
-import ru.practicum.shareit.request.model.ItemRequest;
-
-public interface RequestMapper {
-    ItemRequestDto toDto(ItemRequest itemRequest);
-}
--- src/main/java/ru/practicum/shareit/request/model/RequestItemCreate.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.request.model;
-
-import lombok.Data;
-
-@Data
-public class RequestItemCreate {
-    private String description;
-}
